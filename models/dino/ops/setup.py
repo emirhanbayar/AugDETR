daf7fc1034240973b09824bc8b4a16667d18c86f
emirhanbayar <bayaremirhan07@gmail.com>
2025-01-07 21:08:31 +0300

    fix setup

--- models/dino/ops/setup.py
+++ models/dino/ops/setup.py
@@ -1,20 +1,28 @@
+# ------------------------------------------------------------------------------------------------
+# Deformable DETR
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------------------------------
+# Modified from https://github.com/chengdazhi/Deformable-Convolution-V2-PyTorch/tree/pytorch_1.0.0
+# ------------------------------------------------------------------------------------------------
+
 import os
 import glob
+
 import torch
-from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
-from setuptools import find_packages, setup
+
+from torch.utils.cpp_extension import CUDA_HOME
+from torch.utils.cpp_extension import CppExtension
+from torch.utils.cpp_extension import CUDAExtension
+
+from setuptools import find_packages
+from setuptools import setup
 
 requirements = ["torch", "torchvision"]
 
 def get_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     extensions_dir = os.path.join(this_dir, "src")
-
-    # Create all necessary directories for build
-    os.makedirs(os.path.join(this_dir, "build", "temp.linux-x86_64-cpython-38",
-                            "home", "ebayar", "AugDETR-HAE", "models", "dino", "ops", "src", "cpu"), exist_ok=True)
-    os.makedirs(os.path.join(this_dir, "build", "temp.linux-x86_64-cpython-38",
-                            "home", "ebayar", "AugDETR-HAE", "models", "dino", "ops", "src", "cuda"), exist_ok=True)
 
     main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
     source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
@@ -25,28 +33,20 @@ def get_extensions():
     extra_compile_args = {"cxx": []}
     define_macros = []
 
+
+
     if torch.cuda.is_available() and CUDA_HOME is not None:
         extension = CUDAExtension
         sources += source_cuda
         define_macros += [("WITH_CUDA", None)]
-        
-        extra_compile_args["cxx"] = ["-std=c++14"]
-        nvcc_flags = [
+        extra_compile_args["nvcc"] = [
             "-DCUDA_HAS_FP16=1",
             "-D__CUDA_NO_HALF_OPERATORS__",
             "-D__CUDA_NO_HALF_CONVERSIONS__",
             "-D__CUDA_NO_HALF2_OPERATORS__",
-            "-gencode=arch=compute_70,code=sm_70",
-            "-gencode=arch=compute_75,code=sm_75",
-            "-gencode=arch=compute_80,code=sm_80",
-            "--compiler-bindir=/home/ebayar/anaconda3/envs/dino/bin/x86_64-conda-linux-gnu-gcc",  # Use GCC 9
-            "--expt-relaxed-constexpr",
-            "--extended-lambda",
         ]
-        
-        extra_compile_args["nvcc"] = nvcc_flags
     else:
-        raise NotImplementedError('Cuda is not available')
+        raise NotImplementedError('Cuda is not availabel')
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
